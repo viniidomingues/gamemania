b822f6a48935bac528379a2efdd34a206e113514
viniidomingues <167872665+viniidomingues@users.noreply.github.com>
2024-06-22 14:10:59 -0300

    Add files via upload

---
+++ dist/gamemania/assets/js/login.js
@@ -0,0 +1,12 @@
+// function validarLogin(){
+//     event.preventDefault();
+//     let email = document.getElementById('email').value;
+//     let password = document.getElementById('password').value;
+
+//     if(email.trim() == '' || password.trim() == ''){
+//         document.getElementById('msgValidacao').innerHTML = '<strong>Preencher todos os campos!</strong>';
+//     } else{
+//         console.log(email);
+//         console.log(password);
+//     }   
+// }
